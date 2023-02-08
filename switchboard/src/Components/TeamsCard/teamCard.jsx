619a1dae61e7d79f68ef48a46f31f9d3b52923dc
Nivas S <46569236+Nivashawk@users.noreply.github.com>
2023-02-08 17:38:34 +0530

    team component completed

---
+++ switchboard/src/Components/TeamsCard/teamCard.jsx
@@ -0,0 +1,24 @@
+import './teamCard.css'
+
+
+
+const TeamCard = ({ image, title, description }) => {
+    return (
+        <div style={{
+            height: "fit-content",
+            width: "20%",
+            borderRadius: "10px",
+            display: 'flex',
+            alignItems: "flex-start",
+            justifyContent: "flex-start",
+            flexDirection: "column",
+            gap: "1vh",
+        }}>
+
+            <img className="team-image-container" src={image} alt="" />
+            <div className='team-heading-container'>{title}</div>
+            <div className='team-description-container'>{description}</div>
+        </div>
+    )
+}
+export default TeamCard
--- switchboard/src/Model/Team.content.js
+++ switchboard/src/Model/Team.content.js
@@ -1,28 +1,27 @@
+import team1 from '../Assets/Images/Teams/team1.png'
+import team2 from '../Assets/Images/Teams/team2.png'
+import team3 from '../Assets/Images/Teams/team3.png'
+
 export const teamContent = () => {
     return {
-        heading: "",
-        subheading: "",
-        description : "",
+        heading: "The Team",
+        subheading: "100+ pillars to our Bridge",
+        description : "We have started as a team of 5, now our fire spread across many countries thanks to you!",
         team:[
             {
-                image:"",
-                name:"",
-                content:""
+                image:team1,
+                name:"John.K",
+                content:"Lorem ipsum dolor sit amet consectetur. Vulputate adipiscing egestas lorem."
             },
             {
-                image:"",
-                name:"",
-                content:""
+                image:team2,
+                name:"John.K",
+                content:"Lorem ipsum dolor sit amet consectetur. Vulputate adipiscing egestas lorem."
             },
             {
-                image:"",
-                name:"",
-                content:""
-            },
-            {
-                image:"",
-                name:"",
-                content:""
+                image:team3,
+                name:"John.K",
+                content:"Lorem ipsum dolor sit amet consectetur. Vulputate adipiscing egestas lorem."
             }
         ]
     }
